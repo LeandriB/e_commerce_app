a9191ab2ac5446d64d1d33e3c4132f4acee4e8c9
Leandri Beavis <leandrikuyk@gmail.com>
2022-07-03 14:48:51 -0400

    created POST, PUT and DELETE routes for Category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -57,14 +57,53 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name, 
+  })
+  .then(categoryData => 
+    req.json(categoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: req.params.id,
+  })
+  .then(categoryData => {
+    if(!categoryData) {
+      res.status(404).json({message: 'Category not found with given ID'});
+      return;
+    }
+    res.json(categoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+        id: req.params.id
+        }
+    })
+    .then(categoryData => {
+        if (!categoryData) {
+            res.status(404).json({ message: 'Category not found with given ID' });
+            return;
+        }
+      res.json(categoryData);
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 module.exports = router;
