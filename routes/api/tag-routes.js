c846473be78329358c2c9516626627f69e36eca5
Leandri Beavis <leandrikuyk@gmail.com>
2022-07-03 21:40:11 -0400

    Created POST and PUT routes for Tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,17 +41,42 @@ router.get('/:id', (req, res) => {
     }
   })
   .then(tagData => res.json(tagData))
-  .catch(err);
-  console.log(err);
-  res.status(500).json(err);
+  .catch(err => {
+    res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then(tagData => res.json(tagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(tagData => {
+      if (!tagData){
+        res.status(404).json({
+          message:'No tag found with associated id'
+        });
+      }
+      res.json(tagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
